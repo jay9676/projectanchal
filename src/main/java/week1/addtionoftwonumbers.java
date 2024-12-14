f390891b982dbdd750ae3022dacfb7b20d22262d
ma20282486 <glowingkumar@gmail.com>
2024-12-14 08:58:58 +0530

    intial commit

---
+++ src/main/java/week1/addtionoftwonumbers.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class addtionoftwonumbers {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        addtionoftwonumbers jay = new addtionoftwonumbers();
+        //jay.sum()
+        System.out.println(jay.sum(10,12));
+    }
+}
